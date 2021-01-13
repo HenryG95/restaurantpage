af85196e465d492fa90a819e90dfd221d8e30a4f
Henry <henrygabrielgm@gmail.com>
2021-01-13 06:39:18 -0400

    image testing

--- src/menu_page.js
+++ src/menu_page.js
@@ -4,23 +4,23 @@ const menu = (function() {
     menu.setAttribute('class','menu');
     menu.innerHTML = `
   <figure>
-    <img src='/images/blueberryMuffins.jpeg'/>
+    <img src='./images/blueberryMuffins.jpeg'/>
     <figcaption>Blueberry Muffin</figcaption>
   </figure>
   <figure>
-    <img src ='/images/brownieCake.jpeg'/>
+    <img src ='./images/brownieCake.jpeg'/>
     <figcaption>Brownie Cake</figcaption>
   </figure>
   <figure>
-    <img src ='/images/cheesecake.jpeg'/>
+    <img src ='./images/cheesecake.jpeg'/>
     <figcaption>Cheesecake</figcaption>
   </figure>
   <figure>
-    <img src ='/images/cherryPie.jpeg'/>
+    <img src ='./images/cherryPie.jpeg'/>
     <figcaption>Cherry Pie</figcaption>
   </figure>
   <figure>
-    <img src ='/images/glazedDonuts.jpeg'/>
+    <img src ='./images/glazedDonuts.jpeg'/>
     <figcaption>Glazed Donuts</figcaption>
   </figure>`
     return {menu}
